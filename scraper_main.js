7855cef7254ec99a3e5f4f7eb7c618afe4d5d3e7
ErikRodriguez96 <tuf93296@temple.edu>
2022-04-21 03:17:37 -0400

    re-factoring in progress to accommodate db path

--- scraper_main.js
+++ scraper_main.js
@@ -12,14 +12,26 @@ let config = {
         "Connection": "keep-alive"
     }
 }
-let user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; XBOX_ONE_ED) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-    "PostmanRuntime/7.29.0",
-    "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
-    "Roku4640X/DVP-7.70 (297.70E04154A)",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
-    "Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus Player Build/MMB29T)",
-    "Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Xbox; Xbox One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/13.10586",
+ let user_agents = [
+//     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; XBOX_ONE_ED) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+//     "PostmanRuntime/7.29.0",
+//     "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
+//     "Roku4640X/DVP-7.70 (297.70E04154A)",
+//     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+//     "Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus Player Build/MMB29T)",
+//     "Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Xbox; Xbox One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/13.10586",
+//     "Mozilla/5.0 (Windows; U; Windows NT 9.1; en-US; rv:12.9.1.11) Gecko/20100821 Firefox/70",
+//     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:69.2.1) Gecko/20100101 Firefox/69.2",
+//     "Mozilla/5.0 (Windows NT 6.1; rv:68.7) Gecko/20100101 Firefox/68.7",
+//     "Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0",
+//     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0",
+       "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9b4) Gecko/2008040813 Firefox/3.0b4",
+       "Mozilla/5.0 (X11; U; Linux i686; en-us; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.04 (jaunty) Firefox/3.5",
+     "Mozilla/5.0 (X11; U; Linux x86; rv:1.9.1.1) Gecko/20090716 Linux Firefox/3.5.1",
+      "Mozilla/5.0 (X11; U; Linux i686; es-ES; rv:1.9.1.6) Gecko/20091201 SUSE/3.5.6-1.1.1 Firefox/3.5.6 GTB6"
+
+
+
 ]
 
 // walmart
@@ -27,7 +39,7 @@ const pages = 15
 let counter = 0
 const promises = []
 
-/*
+
 const food_type = [
     "apples",
     "bananas",
@@ -47,9 +59,10 @@ const food_type = [
     "juice",
     "milk",
     "sauce"
-]*/
+]
 let static_types = require('./bazaara-static-types.json')
-let foods = static_types.types
+//let foods = static_types.types
+let foods = food_type
 let stores = static_types.stores
 
 /* Sanity Checks */
@@ -59,17 +72,17 @@ let stores = static_types.stores
 
 //op = axios.get(`walmart_apples.html`)
 //promises.push(op)
-config.headers["user-agent"] = user_agents[getRandomIntInclusive(0,6)]
-let op = axios.get(`https://www.walmart.com/search?q=apples`, config)
-promises.push(op)
+//config.headers["user-agent"] = user_agents[getRandomIntInclusive(0,6)]
+//let op = axios.get(`https://www.walmart.com/search?q=apples`, config)
+//promises.push(op)
 
 //Commented out logic to scrape for foods, uncomment to actually scrape and run
-for (let i = 0, k = 0; i < 1; i++) {
-    k = getRandomIntInclusive(0,6)
+for (let i = 0; i < food_type.length; i++) {
+    let k = getRandomIntInclusive(0, 3)
     for (let j = 1; j < pages; j++) {
-        //rand_header = config.headers["user-agent"] = user_agents[k]
-        //let op = axios.get(`https://www.walmart.com/search?q=${foods[i]}&affinityOverride=store_led&page=${j}`, rand_header)
-        //promises.push(op)
+        config.headers["user-agent"] = user_agents[k]
+        let op = axios.get(`https://www.walmart.com/search?q=${foods[i]}&affinityOverride=store_led&page=${j}`, config)
+        promises.push(op)
     }
 }
 dataset = []
@@ -82,7 +95,7 @@ axios.all(promises).then(axios.spread(async (...args) => {
         const $ = await cheerio.load(res.data);
         //console.log("$ = ", $)
         await $('[data-testid="list-view"]').each((index, element) => {
-            counter+= 1
+            counter += 1
             const rand_upc = getRandomUPC()
             //dataset.push(c(element).text())
             //console.log("element = ", $(element).text())
@@ -111,27 +124,46 @@ axios.all(promises).then(axios.spread(async (...args) => {
                     const rep_count = getRandomIntInclusive(1, 3)
 
                     for (let i = 0; i < rep_count; i++) {
-                        console.log("store_rep_arr = ", store_rep_arr)
+                        //console.log("store_rep_arr = ", store_rep_arr)
                         const rep_idx = getRandomIntInclusive(0, store_rep_arr.length - 1)
-                        console.log("rep_idx = ", rep_idx)
+                        //console.log("rep_idx = ", rep_idx)
                         let cur_store = store_rep_arr[rep_idx]
-                        console.log("cur_store = ", cur_store.name)
+                        //console.log("cur_store = ", cur_store.name)
                         let cur_store_loc = cur_store.locations[getRandomIntInclusive(0, 3)]
-                        console.log("cur_store_loc", cur_store_loc)
+                        //console.log("cur_store_loc", cur_store_loc)
+
+
+                        let rand_percent = getRandomIntInclusive(1, 12) / 100
+
+                        let rep_price = (getRandomIntInclusive(1, 2) === 1) ? price + (price * rand_percent) : price - (price * rand_percent)
                         const store_obj = {
                             "name": cur_store["name"],
                             "latitude": cur_store_loc["lat"],
                             "longtitude": cur_store_loc["lon"]
                         }
                         store_rep_arr.splice(rep_idx, 1)
-                        dataset.push({
+                        //http://localhost:3000/products/add
+                        //Authorization : bazaara-integration-test
+                        /*axios.post('http://localhost:3000/products/add',
+                            {
+                                "name": String(name),
+                                "upc_code": String(rand_upc),
+                                "price": Number(price),
+                                "store": store_obj,
+                                "image_url": String(image_url),
+                                "weight": String(weight)
+                            })*/
+                        let prod_obj = {
                             "name": String(name),
                             "upc_code": String(rand_upc),
-                            "price": Number(price),
+                            "price": Number(rep_price.toFixed(2)),
                             "store": store_obj,
                             "image_url": String(image_url),
                             "weight": String(weight)
-                        })
+                        }
+
+                        dataset.push(prod_obj)
+                        console.log("prod_obj", prod_obj)
                     }
                 }
             }
@@ -139,7 +171,7 @@ axios.all(promises).then(axios.spread(async (...args) => {
             //console.log("product = ", productstring)
         })
     }
-    console.log("dataset final = ", dataset)
+    //console.log("dataset final = ", dataset)
 })).then(() => {
     console.log(`TOTAL RESULTS: ${counter}`)
 }).catch((e) => console.log(e))
@@ -154,20 +186,16 @@ function fetchHtml() {
         });
 }
 
-function checkIfAttributesAreNull(name, weight, price, image_url){
-    if (typeof(name) == "undefined"){
+function checkIfAttributesAreNull(name, weight, price, image_url) {
+    if (typeof (name) === "undefined") {
         return false
-    }
-    else if (typeof(weight) == "undefined"){
+    } else if (typeof (weight) === "undefined") {
         return false
-    }
-    else if (typeof(price) == "undefined"){
+    } else if (typeof(price) !== "number") {
         return false
-    }
-    else if (typeof(image_url) == "undefined"){
+    } else if (typeof (image_url) === "undefined") {
         return false
-    }
-    else{
+    } else {
         return true
     }
 }
@@ -178,13 +206,14 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-function getRandomStoreAndLocation(){
-    const type_select_rand = getRandomIntInclusive(0,3)
-    const coords_select_rand = getRandomIntInclusive(0,3)
+function getRandomStoreAndLocation() {
+    const type_select_rand = getRandomIntInclusive(0, 3)
+    const coords_select_rand = getRandomIntInclusive(0, 3)
     const store_obj = {
         "name": "",
         "latitude": 0.0,
-        "longitude": 0.0 }
+        "longitude": 0.0
+    }
 
     let store_type = stores[type_select_rand].name
     let store_loc_info = stores[type_select_rand].locations[coords_select_rand]
@@ -197,9 +226,10 @@ function getRandomStoreAndLocation(){
     return store_obj
 }
 
-function getRandomUPC(){
-    return getRandomIntInclusive(100000000000,999999999999)
+function getRandomUPC() {
+    return getRandomIntInclusive(100000000000, 999999999999)
 }
+
 // Promise.all(promises).then(() => {
 //     //   results.forEach(value => console.log(value))
 // }).catch((e) => console.log(e))
